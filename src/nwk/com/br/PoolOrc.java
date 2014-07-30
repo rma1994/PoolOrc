517085cab683eb01537993cf6bbd527f8c16f54c
Richard Matheus <Richard Matheus@Pc-Casa>
2014-07-29 22:41:45 -0300

    Primeira Versão:  - Criada as Telas do Programa

---
+++ src/nwk/com/br/PoolOrc.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package nwk.com.br;
+
+import nwk.com.br.form.mainframe;
+
+/**
+ *
+ * @author RMA
+ */
+public class PoolOrc {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        mainframe frame = new mainframe();
+        frame.setVisible(true);
+    }
+    
+}
