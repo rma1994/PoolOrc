a65ca2ac9c8ed7104f972576e05b56aa0e5fbcde
Richard Matheus <richard_matheus2004@yahoo.com.br>
2014-09-23 15:42:42 -0300

    Updates:  - Funções de cadastro de orçamento funcionando(sem inserts ou update)

--- src/nwk/com/br/calculos/OrcamentoCalc.java
+++ src/nwk/com/br/calculos/OrcamentoCalc.java
@@ -5,10 +5,35 @@
  */
 package nwk.com.br.calculos;
 
+import java.math.BigDecimal;
+import java.util.List;
+import nwk.com.br.model.Produto;
+
 /**
  *
  * @author Richard Matheus
  */
 public class OrcamentoCalc {
     
+    public BigDecimal somarProdutos(List<Produto> produtolist){
+        BigDecimal result = new BigDecimal("0");
+        
+        for(Produto produto : produtolist){
+            BigDecimal valor = new BigDecimal(produto.getTotal());
+            result = result.add(valor);
+        }
+        
+        return result;
+    }
+    
+    public BigDecimal valorTotal(String subtotal, String desc){
+        BigDecimal result = new BigDecimal("0");
+        
+        BigDecimal desconto = new BigDecimal(desc);
+        BigDecimal sub = new BigDecimal(subtotal);
+        
+        //Subtrai de subtotal o valor de desconto
+        result = sub.subtract(desconto);
+        return result;
+    }
 }
